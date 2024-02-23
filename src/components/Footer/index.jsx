d627f13ea9d157f29bb222540407a83d89854505
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2024-02-23 11:51:04 -0600

    [Update] Ajustes no tan relevantes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,16 @@
 import { styled } from "styled-components"
 
 const Container = styled.footer`
-    width: 100vw;
+    width: 100%;
     height: 40vh;
     background-color: brown;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-image: url("https://res.cloudinary.com/dejj8n6g7/image/upload/v1708531295/2024/MazeConstructions/banner-con-filtro_igdmpx.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: bottom;
 `
 
 const Footer = () => {
