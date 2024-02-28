11752ea710b82948fab19859eadc7c0d171a3c02
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2024-02-28 13:04:01 -0800

    [Update] Mejore el logo de Maze - se ve mucho mejor

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 const Container = styled.span`
-    width: 420px;
+    width: 360px;
     height: 120px;
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-left: 52px;
+    margin-left: 72px;
 
-    background-image: url("https://res.cloudinary.com/dejj8n6g7/image/upload/v1708620375/2024/MazeConstructions/logo-principal3_gdho7w.svg");    
+    background-image: url("https://res.cloudinary.com/dejj8n6g7/image/upload/v1709153497/2024/MazeConstructions/logo-principal7_um67mr.svg");    
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center center;
