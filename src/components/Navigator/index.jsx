ad52d539f4da25980a97d835cbb4f6168432b745
EduardoRireyes24 <eduardorireyes2024@gmail.com>
2024-02-28 09:59:18 -0800

    [Update] Añadi funcionalidad a los botones para que viajen a cada seccion de la pagina

--- src/components/Navigator/index.jsx
+++ src/components/Navigator/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { styled } from "styled-components"
 
 const Container = styled.nav`
@@ -41,7 +42,7 @@ const ListItem = styled.li`
     }
 `;
 
-const Button = styled.a`
+const Button = styled.button`
     height: 100%;
     color: white;
     background-color: transparent;
@@ -56,24 +57,80 @@ const Button = styled.a`
 
 const Navigator = () => {
 
-    const handleClick = (num) => {
-        console.log(num);
-    }
+    useEffect(() => {
+        const btnScroll1 = document.getElementById("button1");
+        const section1 = document.getElementById("section1");
+
+        const handleClick = (num) => {
+            section1.scrollIntoView({ behavior: 'smooth'});
+        }
+
+        btnScroll1.addEventListener('click', handleClick)
+
+        return () => {
+            btnScroll1.removeEventListener('click', handleClick);
+        }
+    }, [])
+
+    useEffect(() => {
+        const btnScroll2 = document.getElementById("button2");
+        const section2 = document.getElementById("section2");
+
+        const handleClick = (num) => {
+            section2.scrollIntoView({ behavior: 'smooth'});
+        }
+
+        btnScroll2.addEventListener('click', handleClick)
+
+        return () => {
+            btnScroll2.removeEventListener('click', handleClick);
+        }
+    }, [])
+
+    useEffect(() => {
+        const btnScroll3 = document.getElementById("button3");
+        const section3 = document.getElementById("section3");
+
+        const handleClick = (num) => {
+            section3.scrollIntoView({ behavior: 'smooth'});
+        }
+
+        btnScroll3.addEventListener('click', handleClick)
+
+        return () => {
+            btnScroll3.removeEventListener('click', handleClick);
+        }
+    }, [])
+
+    useEffect(() => {
+        const btnScroll4 = document.getElementById("button4");
+        const section4 = document.getElementById("section4");
+
+        const handleClick = (num) => {
+            section4.scrollIntoView({ behavior: 'smooth'});
+        }
+
+        btnScroll4.addEventListener('click', handleClick)
+
+        return () => {
+            btnScroll4.removeEventListener('click', handleClick);
+        }
+    }, [])
 
     return (
         <Container>
             <ContainerList>
                 <ListItem>
-                    <Button href={"#section1"}>Bienvenido</Button>
+                    <Button id={"button1"}>Bienvenido</Button>
                 </ListItem>            
                 <ListItem>
-                    <Button href={"#section2"}>Nuestra Especialidad</Button>
+                    <Button id={"button2"}>Nuestra Especialidad</Button>
                 </ListItem>
                 <ListItem>
-                    <Button href={"#section3"}>Nuestro Equipo</Button>
+                    <Button id={"button3"}>Nuestro Equipo</Button>
                 </ListItem>
                 <ListItem>
-                    <Button href={"#section4"}>Contactanos</Button>
+                    <Button id={"button4"}>Contactanos</Button>
                 </ListItem>
             </ContainerList>
         </Container>
