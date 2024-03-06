9651d6285128d60f13d12f6d5da49711493a32b4
rireyes24 <eduardorireyes2024@gmail.com>
2024-03-05 19:08:17 -0600

    [Update] Solo dire que funciona las validaciones en mi formulario :3

--- src/components/Body/Sections/Contact..jsx
+++ src/components/Body/Sections/Contact..jsx
@@ -4,12 +4,12 @@ import { EmailForm } from "../../EmailForm";
 
 const Container = styled.section`
     width: 100%;
-    height: 62vh;
+    height: 68vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 4% 2%;
+    padding: 34px 2%;
 `;
 
 const ContainerContact = styled.div`
@@ -18,6 +18,7 @@ const ContainerContact = styled.div`
     display: grid;
     grid-template-columns: 48% 4% 48%;
     grid-template-rows: 100%;
+    margin-top: 58px;
 `;
 
 
--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { useForm, ValidationError } from "@formspree/react";
+import { useEffect, useState } from "react";
 
 
 const Container = styled.div`
     width: 80%;
-    height: 90%;    
+    height: 96%;    
     color: #2b2b2b;
     align-self: center;
     justify-self: center;
@@ -12,26 +13,22 @@ const Container = styled.div`
 
 const TextBox = styled.input.attrs({ type: "text"})`
     width: 100%;
-    height: 38px;
+    height: 42px;
     border-radius: 12px;
     background-color: white;
-    border: 2px solid #2b2b2b;
+    border: 1px solid #2b2b2b;
     color: #2b2b2b;
     padding-left: 12px;
-    margin-top: 32px;
+    margin-bottom: 28px;
+    color: #2b2b2b;
 
     &:focus {
-    border-color: red; /* Cambia el color del borde a rojo */
-    outline: none; /* Quita el contorno predeterminado */
-  }
-`;
+        border-color: #009e28; /* Cambia el color del borde a rojo */
+        outline: none; /* Quita el contorno predeterminado */
+        color: #2b2b2b;
+    }
 
-const MiniBox = styled.div`
-    width: 100%;
-    height: 40px;
-    display: flex;    
-    gap: 10px;
-    margin-bottom: 20px;
+   
 `;
 
 const TextArea = styled.input.attrs({ type: "textarea"})`
@@ -39,14 +36,14 @@ const TextArea = styled.input.attrs({ type: "textarea"})`
     height: 88px;
     border-radius: 12px;
     background-color: white;
-    border: 2px solid #2b2b2b;
+    border: 1px solid #2b2b2b;
     color: #2b2b2b;
     padding-left: 12px;
-    margin-top: 32px;
-
+    
     &:focus {
-    border: 1px solid red; /* Cambia el color del borde a rojo */
-  }
+        border: 2px solid #009e28; /* Cambia el color del borde a rojo */
+        outline: none; /* Quita el contorno predeterminado */
+    }
 `;
 
 const SubmitButton = styled.button`
@@ -66,53 +63,126 @@ const SubmitButton = styled.button`
 
 const Line = styled.div`
     width: 2px;
-    height: 80%;
+    height: 88%;
     background-color: #a7a7a7;
     align-self: center;
     justify-self: center;
     border-radius: 50px;
 `;
 
-function ContactMessage(){
-    const [state, handleSubmit] = useForm("xgegygrw");
+const FormContainer = styled.form`
+    width: 100%;
+    height:420px;
+    display: flex;
+    flex-direction: column;
+    margin-top: 62px;
+`;
+
+const EmailForm = () => {
+
+    const [nameClient, setNameClient] = useState("");
+    const [email, setEmail] = useState("");
+    const [affair, setAffair] = useState("");
+    const [message, setMessage] = useState("");
+
+    const [msjSended, setMsjSended] = useState(false);
+
+    const [hasContentName, setHasContentName] = useState(false);
+    const [hasContentEmail, setHasContentEmail] = useState(false);
+    const [hasContentMessage, setHasContentMessage] = useState(false);
+
+
+    function ValidationTextBox(){
+        if(nameClient.length > 1){
+            setHasContentName(false);  
+        } else{
+            setHasContentName(true);
+        }
+
+        if(email.length > 1){
+            setHasContentEmail(false);
+        }else{
+            setHasContentEmail(true);
+        }
+
+        if(message.length > 1){
+            setHasContentMessage(false);            
+        }else{
+            setHasContentMessage(true);
+        }
+    };
+
+    function ClearForm(){
+        setNameClient("");
+        setEmail("");
+        setAffair("");
+        setMessage("");
+    }
+
+    const sendMessage = async (event)  => {
+        event.preventDefault();
+        ValidationTextBox();
+
+        try {
+            if(nameClient.length > 1 && email.length > 1 && message.length > 1){
+                setMsjSended(true);
+                ClearForm();
+            }          
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+        }
+    };
 
     return(
         <>
             <Container>
                 <h3>Envianos un correo para agendar una cita </h3>
-                <form onSubmit={handleSubmit} method="POST">                   
-                    <TextBox placeholder="Nombre" id="name" type="text" name="name"></TextBox>
-                    <ValidationError 
-                        prefix="Name" 
-                        field="name" 
-                        errors={state.errors}
-                    ></ValidationError>
+                <FormContainer onSubmit={sendMessage} method="POST">  
+                    <TextBox 
+                        placeholder="Nombre*" 
+                        id="name" 
+                        type="text" 
+                        name="name"
+                        value={nameClient}
+                        onChange={(e) => setNameClient(e.target.value)}
+                    ></TextBox>
+                    {hasContentName ? <p>OBLIGATORIO: Por favor ingrese su nombre!</p> : <></>}
+                   
+                   <TextBox 
+                        placeholder="Email*" 
+                        id="email" 
+                        type="email" 
+                        name="email" value={email}
+                        onChange={(e) => setEmail(e.target?.value)}
+                    ></TextBox>
+                     {hasContentEmail ? <p>OBLIGATORIO: Por favor ingrese su correo!</p> : <></>}
 
-                    <TextBox placeholder="Asunto" id="affair" type="text" name="affair"></TextBox>
-                    <ValidationError 
-                        prefix="Affair" 
-                        field="affair" 
-                        errors={state.errors}
-                    ></ValidationError>
-
-                    <TextBox placeholder="Email" id="email" type="email" name="email"></TextBox>
-                    <ValidationError 
-                        prefix="Email" 
-                        field="email" 
-                        errors={state.errors}
-                    ></ValidationError>
-                    {/* <TextBox placeholder="Asunto"></TextBox> */}
-                    <TextArea placeholder="Mensaje" id="message" name="message"></TextArea>
-                    <ValidationError 
-                        prefix="Message" 
-                        field="message" 
-                        errors={state.errors}
-                    ></ValidationError>
-                    <SubmitButton type="submit" disabled={state.submitting}>ENVIAR</SubmitButton>
-                    {state.succeeded ? <p>Mensaje Enviado, Pronto nos contactaremos con usted</p> : <></> }
-                </form>
+                    <TextBox 
+                        placeholder="Asunto" 
+                        id="affair" 
+                        type="text" 
+                        name="affair"
+                        value={affair}
+                        onChange={(e) => setAffair(e.target.value)}
+                    ></TextBox>
+                                     
+                    <TextArea 
+                        placeholder="Mensaje*" 
+                        id="message" 
+                        name="Mensaje" 
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    ></TextArea>
+                    {hasContentMessage ? <p>OBLIGATORIO: Por favor escriba un mensaje!</p> : <></>}
+                   
+                    <SubmitButton type="submit">ENVIAR</SubmitButton>
+                    {msjSended ? <p>Mensaje Enviado, Pronto nos contactaremos con usted</p> : <></> }
+                </FormContainer>
             </Container>
+
             <Line></Line>
+
             <Container>
                 <h3>Envianos un mensaje al indox o siguenos en nuestras redes sociales</h3>
             </Container>
@@ -120,13 +190,20 @@ function ContactMessage(){
     );
 }
 
-const EmailForm = () => {
-
-   return(
-    <>
-        <ContactMessage></ContactMessage>
-    </>
-   )
-}
-
 export { EmailForm }
+
+
+// const response = await fetch('https://formspree.io/xgegygrw', {
+//               method: 'POST',
+//               headers: {
+//                 'Content-Type': 'application/json',
+//               },
+//               body: JSON.stringify({ nameClient, email, affair, message }),
+//             });
+      
+//             if (response.ok) {
+//               setMsjSended(true);
+//               ClearForm();
+//             } else {
+//               alert('Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+//             }
