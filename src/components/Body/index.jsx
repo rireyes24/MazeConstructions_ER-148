57066207afac303ee2f68c63aa3470b55fc1575c
rireyes24 <eduardorireyes2024@gmail.com>
2024-03-07 14:53:41 -0600

    [Update/Responsive] Ajuste la seccion de Nuestro Team

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,7 +1,6 @@
 import { styled } from "styled-components"
 import { Title } from "../../components/Title"
 import { Description } from "../../components/Description"
-import { MazeImage } from "../../components/Image"
 import WelcomeToMaze  from "./Sections/WelcomeToMaze"
 import { Specialty } from "./Sections/OurSpeciality"
 import { Location } from "./Sections/OurLocation"
@@ -30,6 +29,11 @@ const Team = styled(Sections)`
     height: 630px;        
     justify-content: space-around;
     padding: 34px 0px;
+
+    @media(max-width: 425px) {        
+        height: 860px;
+        padding: 0px 0px;
+    }
 `;
 
 const BoxTeam = styled.div`
@@ -46,12 +50,31 @@ const BoxTeam = styled.div`
     @media(max-width: 425px) {        
         display: flex;
         flex-direction: column;
+        justify-content: space-around;
+        height: 620px;
+        margin-top: 0px;
     }
 `;
 
+const MazeImage = styled.img`    
+    width: 280px;
+    height: 220px;
+    border-radius: 14px;
+    object-fit: cover;    
+    align-self: center;
+    justify-self: center;
 
+    -webkit-box-shadow: 3px 2px 18px -3px #1b1b1b;
+    -moz-box-shadow: 3px 2px 18px -3px #1b1b1b;
+    box-shadow: 3px 2px 18px -3px #1b1b1b;
 
-
+    @media(max-width: 425px) {        
+        width: 280px;
+        height: 180px;
+        margin-bottom: 0px;
+        border-radius: 10px;
+    }
+`;
 
 export default function Main () {
     return (
@@ -62,26 +85,23 @@ export default function Main () {
            
             <Team id="section3">
                 <Title>Nuestro Equipo</Title>
-                <Description width={"66%"}>
+                <Description>
                     Nuestro <b>equipo altamente calificado</b> de arquitectos, ingenieros y constructores trabaja 
                     de forma <b>profesional</b> y <b>personalizada</b> para que se ajusten a las necesidades específicas
                      de cada cliente.
                 </Description>
                 <BoxTeam>
                     <MazeImage
-                        url="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551688/2024/MazeConstructions/team-photo1_dtixcs.jpg" 
+                        src="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551688/2024/MazeConstructions/team-photo1_dtixcs.jpg" 
                         name="Tractor Naranja"
-                        width="380px"
                     />
                     <MazeImage
-                        url="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551688/2024/MazeConstructions/team-photo2_nfz6xd.jpg" 
+                        src="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551688/2024/MazeConstructions/team-photo2_nfz6xd.jpg" 
                         name="Tractor Naranja"
-                        width="260px"
                     />
                     <MazeImage
-                        url="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551858/2024/MazeConstructions/team-photo3_kj4qqv.jpg" 
+                        src="https://res.cloudinary.com/dejj8n6g7/image/upload/v1708551858/2024/MazeConstructions/team-photo3_kj4qqv.jpg" 
                         name="Tractor Naranja"
-                        width="260px"
                     />
                 </BoxTeam>
             </Team>
