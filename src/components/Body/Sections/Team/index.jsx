2a26b601f53cfe1b62cac757bbb3f5a0aa498199
rireyes24 <eduardorireyes2024@gmail.com>
2024-03-11 16:38:39 -0600

    [Update] Añadi la funcionalidad de seleccion de idioma en la seccion Team

--- src/components/Body/Sections/Team/index.jsx
+++ src/components/Body/Sections/Team/index.jsx
@@ -3,16 +3,26 @@ import { Title } from "../../../Title"
 import { Description } from "../../../Description"
 import { dataTeam } from "../../../../data/dataTeam"
 
-const Team = () => {
+const Team = (props) => {
     return(
         <>
             <TeamContainer id="section3">
-                <Title>Nuestro Equipo</Title>
-                <Description>
-                    Nuestro <b>equipo altamente calificado</b> de arquitectos, ingenieros y constructores trabaja 
-                    de forma <b>profesional</b> y <b>personalizada</b> para que se ajusten a las necesidades específicas
-                     de cada cliente.
-                </Description>
+                {props.selectLanguage === "ES" ? 
+                    <Title>Nuestro Equipo</Title>:
+                    <Title>Our Team</Title>
+                }
+                {props.selectLanguage === "ES" ? 
+                    <Description>
+                        Nuestro <b>equipo altamente calificado</b> de arquitectos, ingenieros y constructores trabaja 
+                        de forma <b>profesional</b> y <b>personalizada</b> para que se ajusten a las necesidades específicas
+                        de cada cliente.
+                    </Description>:
+                    <Description>
+                        Our <b>highly qualified team</b> of architects, engineers, and builders works in 
+                        a <b>professional</b> and <b>personalized</b> manner to tailor to the specific needs 
+                        of each client.
+                    </Description>
+                }
                 <BoxTeam>
                     {dataTeam.map((item) => {
                         return(
