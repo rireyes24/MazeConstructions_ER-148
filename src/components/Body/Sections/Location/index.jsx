f918fbff9010646a7368cbf3951a5e7c645a2628
rireyes24 <eduardorireyes2024@gmail.com>
2024-04-11 12:12:53 -0600

    [Update] Añadi la funcionalidad de idioma en la secciones Team & Locations

--- src/components/Body/Sections/Location/index.jsx
+++ src/components/Body/Sections/Location/index.jsx
@@ -3,6 +3,9 @@ import { Title } from "../../../Title";
 import { dataLocation } from "../../../../data/dataLocation";
 
 const Location = (props) => {
+
+    const ourLocations = props.dataLanguage.data.body.sections.location.locations;
+
     return(
         <ContainerLocation id="section4">
                 
@@ -11,12 +14,12 @@ const Location = (props) => {
                     <Title>Our Locations</Title>
                 }
                 <Box>
-                    {dataLocation.map((data) => {
+                    {ourLocations.map((data) => {
                         return(
                             <MiniBox href={data.map} target="_blank">
                                 <MiniBoxData>
-                                    <City>{data.city}</City>
-                                    <Phone>{data.telephone}</Phone>
+                                    <City>{data.city.toUpperCase()}</City>
+                                    <Phone>TEL: {data.telephone}</Phone>
                                     <Address>{data.address}</Address>
                                 </MiniBoxData>
                                 <MazeImage
--- src/components/Body/Sections/Team/index.jsx
+++ src/components/Body/Sections/Team/index.jsx
@@ -7,22 +7,10 @@ const Team = (props) => {
     return(
         <>
             <TeamContainer id="section3">
-                {props.selectLanguage === "ES" ? 
-                    <Title>Nuestro Equipo</Title>:
-                    <Title>Our Team</Title>
-                }
-                {props.selectLanguage === "ES" ? 
-                    <Description>
-                        Nuestro <b>equipo altamente calificado</b> de arquitectos, ingenieros y constructores trabaja 
-                        de forma <b>profesional</b> y <b>personalizada</b> para que se ajusten a las necesidades específicas
-                        de cada cliente.
-                    </Description>:
-                    <Description>
-                        Our <b>highly qualified team</b> of architects, engineers, and builders works in 
-                        a <b>professional</b> and <b>personalized</b> manner to tailor to the specific needs 
-                        of each client.
-                    </Description>
-                }
+
+                <Title>{props.dataLanguage.data.body.sections.team.title}</Title>
+                <Description>{props.dataLanguage.data.body.sections.team.description}</Description>
+                
                 <BoxTeam>
                     {dataTeam.map((item) => {
                         return(
